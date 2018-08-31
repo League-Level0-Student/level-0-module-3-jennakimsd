d283edc2f826fffa1a13d2af547a255efa658d58
jennakimsd <League@AdministorsiMac.attlocal.net>
2018-08-31 16:57:31 -0700

    I did Obedient Robot and 99 Bottles of Beer on the Wall

--- src/obedientRobot.java
+++ src/obedientRobot.java
@@ -1,14 +1,61 @@
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class obedientRobot {
+	static Robot bob= new Robot();
 public static void main(String[] args) {
-	}
-static Robot bob= new Robot();
+	bob.setSpeed(10);
+	bob.penDown();
+	String color= JOptionPane.showInputDialog("What color would you like the shape to be?");
+		if (color.equals("pink")) {
+			bob.setPenColor(Color.pink);
+		}
+		else if(color.equals("cyan")) {
+			bob.setPenColor(Color.cyan);
+		}
+		else if (color.equals("green")) {
+			bob.setPenColor(Color.green);
+		}
+		else if (color.equals("magenta")) {
+			bob.setPenColor(Color.magenta);
+		}
+		else {
+			bob.setRandomPenColor();
+		}
+	String shape= JOptionPane.showInputDialog("What shape do you want? (triangle, circle, or square?)");
+		if (shape.equals("triangle")) {
+			drawTriangle();
+		}
+		else if (shape.equals("circle")) {
+			drawCircle();
+		}
+		else if (shape.equals("square")) {
+			drawSquare();
+		}
+}
+
 public static void drawSquare() {
 	for (int i = 0; i < 4; i++) {
-		bob.penDown();
-		bob.move(100);
-		bob.turn(90);}
+		bob.move(200);
+		bob.turn(90);
+		}
+	}
+public static void drawTriangle() {
+	for(int i = 0; i < 3; i++) {
+		bob.move(200);
+		bob.turn(120);
+		}
+	}
+public static void drawCircle() {	
+	for(int i= 0; i< 360; i++) {
+		bob.move(1);
+		bob.turn(1);	
+		}
 	}
 
+
 }
+
